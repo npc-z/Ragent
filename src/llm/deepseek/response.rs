795edd2ed72b3678b40cd90afb611dee770d5736
npc-z <1763998996@qq.com>
2026-06-24 17:19:44 +0800

    mute some `dead code` for now

--- src/llm/deepseek/response.rs
+++ src/llm/deepseek/response.rs
@@ -1,22 +1,21 @@
 use serde::Deserialize;
 
 use crate::llm::deepseek::enums::finish_reason::FinishReason;
-use crate::llm::deepseek::enums::model::DeepseekModel;
 use crate::llm::response::{ApiResponse, ResponseMessage};
 use crate::tool_call::tool::ToolCall;
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct DeepseekResponse {
-    id: String,
-    created: u64,
-    model: DeepseekModel,
-    system_fingerprint: String,
+    // id: String,
+    // created: u64,
+    // model: DeepseekModel,
+    // system_fingerprint: String,
     choices: Vec<Choice>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 struct Choice {
-    index: u16,
+    // index: u16,
     finish_reason: FinishReason,
     message: ResponseMessage,
 }
